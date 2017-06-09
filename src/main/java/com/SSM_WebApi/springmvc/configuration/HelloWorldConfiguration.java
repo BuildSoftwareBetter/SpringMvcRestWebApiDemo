d9ea231654839441f7cdc0b95ab0760f2386b8f8
david <469575628@qq.com>
2017-06-09 16:24:01 +0800

    create pro

---
+++ src/main/java/com/SSM_WebApi/springmvc/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,12 @@
+package com.SSM_WebApi.springmvc.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.SSM_WebApi.springmvc")
+public class HelloWorldConfiguration {
+
+}
